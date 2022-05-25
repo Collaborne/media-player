8f38d11e6dddc795927d9ea1c36a908325101654
Ion Bragaru <ion.bragaru@devexperts.com>
2022-05-25 17:39:58 +0300

    PR fixes

--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -3,6 +3,7 @@ import { FC, useCallback, useMemo } from 'react';
 import { SliderProps } from '@mui/material/Slider/Slider';
 import { ProgressBarStyled } from './ProgressBarStyled';
 import { useVideo } from '../../hooks';
+import { PROGRESS_BAR_DIVIDER } from '../../utils/constants';
 
 // TODO: Extend for highlights/tracks
 interface ProgressBarProps extends SliderProps {}
@@ -10,14 +11,16 @@ interface ProgressBarProps extends SliderProps {}
 export const ProgressBar: FC<ProgressBarProps> = props => {
 	const { api } = useVideo();
 
-	const onVolumeChangeHandler = useCallback(
+	const onVolumeChange = useCallback(
 		(e: Event, newValue: number | number[], _activeThumb: number) => {
 			e.preventDefault();
 			if (Array.isArray(newValue)) {
 				return;
 			}
-			const timeForJump = (newValue / 100) * (api?.getDuration?.() || 0);
-			api?.setCurrentTime?.(timeForJump);
+			// Get new time according to played time from the total video duration
+			const seekTime =
+				(newValue / PROGRESS_BAR_DIVIDER) * (api?.getDuration?.() || 0);
+			api?.setCurrentTime?.(seekTime);
 		},
 		[api?.setCurrentTime, api?.getDuration],
 	);
@@ -25,15 +28,20 @@ export const ProgressBar: FC<ProgressBarProps> = props => {
 	const value = useMemo(() => {
 		const videoDuration = api?.getDuration?.();
 		const currentTime = api?.getCurrentTime?.();
+
+		// Calculate current slider's "rail" value according to total duration
+		// and current played time
 		if (videoDuration && currentTime) {
-			return (currentTime / videoDuration) * 100;
+			return (currentTime / videoDuration) * PROGRESS_BAR_DIVIDER;
 		}
 		return 0;
 	}, [api?.getDuration, api?.getCurrentTime]);
 
 	return (
 		<ProgressBarStyled
-			onChange={onVolumeChangeHandler}
+			min={0}
+			max={PROGRESS_BAR_DIVIDER}
+			onChange={onVolumeChange}
 			value={value}
 			{...props}
 		/>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const MIN_VOLUME = 50;
 export const PLAYBACK_RATES = [1, 1.2, 1.5, 1.7, 2];
 export const OVERLAY_HIDE_DELAY = 1000;
+export const PROGRESS_BAR_DIVIDER = 100;
