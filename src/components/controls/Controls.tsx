50b53fd5449dd6284691758308ba47bdf37d1232
Ion Bragaru <ion.bragaru@devexperts.com>
2022-05-30 23:48:44 +0300

    Adding animation to play/pause events

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -7,6 +7,10 @@ import { CenteredPlayButton } from '../centered-play-button/CenteredPlayButton';
 import { useControlsStyles } from './useControlsStyles';
 import { ProgressBar } from '../progress-bar/ProgressBar';
 import { BottomControlPanel } from '../bottom-control-panel/BottomControlPanel';
+import { BigPauseIcon } from '../icons/BigPauseIcon';
+import { BigPlayIcon } from '../icons/BigPlayIcon';
+
+const PLAY_PAUSE_ANIMATION_DURATION = 300;
 
 type ControlProps = {
 	isVisible?: boolean;
@@ -19,15 +23,56 @@ export const Controls: FC<ControlProps> = ({ isVisible }) => {
 	const [hasStarted, setHasStarted] = useState<boolean>(
 		Boolean(api?.getPlaying?.()),
 	);
+	const [showPlayAnimation, setShowPlayAnimation] = useState(false);
+	const [showPauseAnimation, setShowPauseAnimation] = useState(false);
+
 	// Added TS for api as any, because it is also a event listener,
 	// that this hook looks for
 	useEventListener('play', () => setHasStarted(true), api as any);
 
-	// Controls styles
-	const { wrapper, wrapperBottomPanel } = useControlsStyles();
+	// Play animation when video is paused
+	useEventListener(
+		'pause',
+		() => {
+			if (!hasStarted) {
+				return;
+			}
+			setShowPauseAnimation(true);
+			setTimeout(
+				() => setShowPauseAnimation(false),
+				PLAY_PAUSE_ANIMATION_DURATION,
+			);
+		},
+		api as any,
+	);
 
+	// Play animation when video is played(exception: prePlay state)
+	useEventListener(
+		'play',
+		() => {
+			if (!hasStarted) {
+				return;
+			}
+			setShowPlayAnimation(true);
+			setTimeout(
+				() => setShowPlayAnimation(false),
+				PLAY_PAUSE_ANIMATION_DURATION,
+			);
+		},
+		api as any,
+	);
+
+	// Controls styles
+	const { wrapper, wrapperBottomPanel, bigCenteredIcon } = useControlsStyles();
 	return (
 		<div className={wrapper}>
+			{showPauseAnimation && (
+				<BigPauseIcon width={90} height={90} className={bigCenteredIcon} />
+			)}
+			{showPlayAnimation && (
+				<BigPlayIcon width={90} height={90} className={bigCenteredIcon} />
+			)}
+
 			{!hasStarted ? (
 				<>
 					<CenteredPlayButton />
--- src/components/controls/useControlsStyles.ts
+++ src/components/controls/useControlsStyles.ts
@@ -1,6 +1,16 @@
 import { makeStyles } from '@mui/styles';
 
+// TODO: Integrate themes and set duration as in PLAY_PAUSE_ANIMATION_DURATION
 export const useControlsStyles = makeStyles(() => ({
+	'@keyframes fadeOut': {
+		from: {
+			opacity: 0.8,
+		},
+		to: {
+			opacity: 0,
+			transform: 'translate(-50%, -50%) scale(1.2)',
+		},
+	},
 	wrapper: {
 		width: '100%',
 		height: '100%',
@@ -13,5 +23,16 @@ export const useControlsStyles = makeStyles(() => ({
 		position: 'absolute',
 		bottom: 0,
 		width: '100%',
+	},
+	bigCenteredIcon: {
+		margin: '0',
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%) scale(1)',
+		width: '80px',
+		height: '80px',
+		animation: '$fadeOut 300ms ease',
+		animationIterationCount: 1,
 	},
 }));
