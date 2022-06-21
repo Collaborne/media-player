ad61f90819643e8abefa542591910eb096b9b968
Ion Bragaru <ion.bragaru@devexperts.com>
2022-06-21 20:14:43 +0300

    Unnecessary SSR checks removal

--- src/utils/dom-target.ts
+++ src/utils/dom-target.ts
@@ -1,5 +1,3 @@
-import Bowser from 'bowser';
-
 import type { MutableRefObject } from 'react';
 type TargetValue<T> = T | undefined | null;
 
@@ -10,19 +8,10 @@ export type BasicTarget<T extends TargetType = Element> =
 	| TargetValue<T>
 	| MutableRefObject<TargetValue<T>>;
 
-// TODO: Check bowser lib for getting any type of browser
-
-export const browser = Bowser.getParser(window.navigator.userAgent);
-export const isBrowser = Boolean(browser.getBrowser());
-
 export const getTargetElement = <T extends TargetType>(
 	target: BasicTarget<T>,
 	defaultElement?: T,
 ): TargetValue<T> => {
-	if (!isBrowser) {
-		return undefined;
-	}
-
 	if (!target) {
 		return defaultElement;
 	}
