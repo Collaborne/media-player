0340ff3d922f572402141fd6026605c04c7791ec
Mehdi Moghaddam <moghaddam.m01@gmail.com>
2022-06-29 07:49:03 +0430

    Upgrade dependecies Refactor @mui/styles to tss-react

--- src/components/pip-overlay/PipOverlay.tsx
+++ src/components/pip-overlay/PipOverlay.tsx
@@ -30,7 +30,7 @@ export const PipOverlay: FC<PipOverlayProps> = () => {
 		centerButtonIcon,
 		playBackRateWrapper,
 		playbackTypography,
-	} = usePipOverlayStyles();
+	} = usePipOverlayStyles().classes;
 	const [showControls, setShowControls] = useState(true);
 	const [lastMouseLeave, setLastMouseLeave] = useState<number>(0);
 	const [lastMouseMove, setLastMouseMove] = useState<number>(0);
