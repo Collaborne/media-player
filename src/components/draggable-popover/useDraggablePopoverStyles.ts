d13ff6bfc0cbacb001f2957934dfa18699ba804e
Mehdi Moghaddam <moghaddam.m01@gmail.com>
2022-06-30 11:57:44 +0430

    Format code

--- src/components/draggable-popover/useDraggablePopoverStyles.ts
+++ src/components/draggable-popover/useDraggablePopoverStyles.ts
@@ -4,40 +4,41 @@ interface UseDraggablePopoverStylesProps {
 	isExpanded: boolean;
 }
 
-export const useDraggablePopoverStyles = makeStyles<UseDraggablePopoverStylesProps>()((theme, {isExpanded}) => ({
-	paper: {
-		height: '100%',
-		width: '100%',
-		display: 'inline-block',
-		position: !isExpanded ? 'sticky' : 'initial',
-		zIndex: 9999,
-		pointerEvents: 'auto',
-		overflow: 'hidden',
-		margin: 0,
-	},
-	portalWrapper: {
-		height: isExpanded ? 'inherit' : `calc(100vh - ${theme.spacing(4)})`,
-		width: isExpanded ? '100%' : `calc(100vw - ${theme.spacing(4)})`,
-		zIndex: isExpanded ? 0 : 9999,
-		background: 'transparent',
-		position: isExpanded ? 'relative' : 'fixed',
-		top: isExpanded ? 0 : theme.spacing(2),
-		left: isExpanded ? 0 : theme.spacing(2),
-		pointerEvents: 'none',
-		padding: 0,
-		margin: 0,
-	},
-	progressBar: {
-		position: 'absolute',
-		bottom: 0,
-		borderRadius: 'unset',
-
-		'& .MuiSlider-thumb': {
-			width: theme.spacing(0.75),
-			height: theme.spacing(0.75),
+export const useDraggablePopoverStyles =
+	makeStyles<UseDraggablePopoverStylesProps>()((theme, { isExpanded }) => ({
+		paper: {
+			height: '100%',
+			width: '100%',
+			display: 'inline-block',
+			position: !isExpanded ? 'sticky' : 'initial',
+			zIndex: 9999,
+			pointerEvents: 'auto',
+			overflow: 'hidden',
+			margin: 0,
 		},
-	},
-	resizeSquares: {
-		pointerEvents: 'auto',
-	},
-}));
+		portalWrapper: {
+			height: isExpanded ? 'inherit' : `calc(100vh - ${theme.spacing(4)})`,
+			width: isExpanded ? '100%' : `calc(100vw - ${theme.spacing(4)})`,
+			zIndex: isExpanded ? 0 : 9999,
+			background: 'transparent',
+			position: isExpanded ? 'relative' : 'fixed',
+			top: isExpanded ? 0 : theme.spacing(2),
+			left: isExpanded ? 0 : theme.spacing(2),
+			pointerEvents: 'none',
+			padding: 0,
+			margin: 0,
+		},
+		progressBar: {
+			position: 'absolute',
+			bottom: 0,
+			borderRadius: 'unset',
+
+			'& .MuiSlider-thumb': {
+				width: theme.spacing(0.75),
+				height: theme.spacing(0.75),
+			},
+		},
+		resizeSquares: {
+			pointerEvents: 'auto',
+		},
+	}));
