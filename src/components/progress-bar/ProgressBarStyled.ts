6863bb3efdaf4bb43962b7f73f290640d4ccb0bc
Mehdi Moghaddam <moghaddam.m01@gmail.com>
2022-07-03 13:03:05 +0430

    Fix typescript typing error (TS4023)

--- src/components/progress-bar/ProgressBarStyled.ts
+++ src/components/progress-bar/ProgressBarStyled.ts
@@ -1,48 +1,50 @@
-import Slider from '@mui/material/Slider';
-import { styled } from '@mui/material/styles';
+import { Slider, SliderProps, styled } from '@mui/material';
+import { StyledComponent } from '@mui/styled-engine';
 
-export const ProgressBarStyled = styled(Slider)(({ theme }) => ({
-	'&.MuiSlider-root': {
-		padding: `${theme.spacing(1.5)} 0 0 0`,
-		pointerEvents: 'auto',
-	},
-
-	'& .MuiSlider-thumb': {
-		height: theme.spacing(1.25),
-		width: theme.spacing(1.25),
-		backgroundColor: theme.palette.primary.main,
-		marginTop: theme.spacing(0.625),
-		marginLeft: 0,
-		'&:focus, &:hover, &$active': {
-			boxShadow: 'inherit',
+export const ProgressBarStyled: StyledComponent<SliderProps> = styled(Slider)(
+	({ theme }) => ({
+		'&.MuiSlider-root': {
+			padding: `${theme.spacing(1.5)} 0 0 0`,
+			pointerEvents: 'auto',
 		},
 
-		opacity: 0,
-		transition: '.3s opacity ease-out',
-	},
+		'& .MuiSlider-thumb': {
+			height: theme.spacing(1.25),
+			width: theme.spacing(1.25),
+			backgroundColor: theme.palette.primary.main,
+			marginTop: theme.spacing(0.625),
+			marginLeft: 0,
+			'&:focus, &:hover, &$active': {
+				boxShadow: 'inherit',
+			},
 
-	'&:hover .MuiSlider-thumb': {
-		opacity: 1,
-		'&:hover': {
-			boxShadow: `0px 0px 0px ${theme.spacing(0.375)} rgb(203 1 255 / 16%)`,
+			opacity: 0,
+			transition: '.3s opacity ease-out',
 		},
-	},
 
-	'& .MuiSlider-track': {
-		height: theme.spacing(0.75),
-		border: 0,
-		color: theme.palette.primary.main,
-		borderRadius: 0,
-		transform: 'unset',
-		top: 'unset',
-		bottom: 0,
-	},
-	'& .MuiSlider-rail': {
-		bottom: 0,
-		height: theme.spacing(0.75),
-		color: theme.palette.grey[300],
-		transform: 'unset',
-		borderRadius: 0,
-		top: 'unset',
-	},
-}));
+		'&:hover .MuiSlider-thumb': {
+			opacity: 1,
+			'&:hover': {
+				boxShadow: `0px 0px 0px ${theme.spacing(0.375)} rgb(203 1 255 / 16%)`,
+			},
+		},
+
+		'& .MuiSlider-track': {
+			height: theme.spacing(0.75),
+			border: 0,
+			color: theme.palette.primary.main,
+			borderRadius: 0,
+			transform: 'unset',
+			top: 'unset',
+			bottom: 0,
+		},
+		'& .MuiSlider-rail': {
+			bottom: 0,
+			height: theme.spacing(0.75),
+			color: theme.palette.grey[300],
+			transform: 'unset',
+			borderRadius: 0,
+			top: 'unset',
+		},
+	}),
+);
