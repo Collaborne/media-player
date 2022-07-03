fd62cc0237c21f4bc99365844f8e2e1f20fda4e0
Mehdi Moghaddam <moghaddam.m01@gmail.com>
2022-07-03 13:03:05 +0430

    Fix typescript typing error (TS4023)

--- src/components/bottom-control-panel/components/VolumeBarStyled.ts
+++ src/components/bottom-control-panel/components/VolumeBarStyled.ts
@@ -1,28 +1,31 @@
-import { Slider, styled } from '@mui/material';
+import { Slider, SliderProps, styled } from '@mui/material';
+import { StyledComponent } from '@mui/styled-engine';
 
-export const VolumeBarStyled = styled(Slider)(({ theme }) => ({
-	'&.MuiSlider-root': {
-		padding: 0,
-		width: theme.spacing(8),
-	},
-
-	'& .MuiSlider-thumb': {
-		height: theme.spacing(0.75),
-		width: theme.spacing(0.75),
-		background: theme.palette.text.secondary,
-
-		'&:focus, &:hover, &$active': {
-			boxShadow: 'inherit',
+export const VolumeBarStyled: StyledComponent<SliderProps> = styled(Slider)(
+	({ theme }) => ({
+		'&.MuiSlider-root': {
+			padding: 0,
+			width: theme.spacing(8),
 		},
-	},
 
-	'& .MuiSlider-track': {
-		height: theme.spacing(0.5),
-		border: 0,
-		background: theme.palette.text.secondary,
-	},
-	'& .MuiSlider-rail': {
-		height: theme.spacing(0.5),
-		background: theme.palette.text.disabled,
-	},
-}));
+		'& .MuiSlider-thumb': {
+			height: theme.spacing(0.75),
+			width: theme.spacing(0.75),
+			background: theme.palette.text.secondary,
+
+			'&:focus, &:hover, &$active': {
+				boxShadow: 'inherit',
+			},
+		},
+
+		'& .MuiSlider-track': {
+			height: theme.spacing(0.5),
+			border: 0,
+			background: theme.palette.text.secondary,
+		},
+		'& .MuiSlider-rail': {
+			height: theme.spacing(0.5),
+			background: theme.palette.text.disabled,
+		},
+	}),
+);
