84572ef39ee883df548136a4c7240bc4503a8167
Ion Bragaru <ion.bragaru@devexperts.com>
2022-07-07 09:17:33 +0300

    Import fix

--- .storybook/decorators/with-player-theme.tsx
+++ .storybook/decorators/with-player-theme.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@mui/material';
 import { deepmerge } from '@mui/utils';
 import { StoryContext } from '@storybook/addons';
 import { FC } from 'react';
+
 import { playerTheme } from '../../src/theme';
 
 export const withPlayerTheme = (
