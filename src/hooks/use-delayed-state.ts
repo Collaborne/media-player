635dcc49b3952605198329788097a900eb526682
Oluwaseun Jolaoso <sudhons@gmail.com>
2022-09-28 12:47:35 +0100

    Memoize useDelayState hook's callbacks
    Fixes https://github.com/Collaborne/backlog/issues/1192

--- src/hooks/use-delayed-state.ts
+++ src/hooks/use-delayed-state.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useCallback, useEffect, useState, useRef } from 'react';
 
 /** Set a state with delay */
 export const useDelayedState = <T>(
@@ -7,7 +7,7 @@ export const useDelayedState = <T>(
 	const [state, setState] = useState<T>(initialState);
 	const timeoutRef = useRef<NodeJS.Timeout | null>();
 
-	const setStateAfter = (newState: T, delay = 0) => {
+	const setStateAfter = useCallback((newState: T, delay = 0) => {
 		if (delay === 0 || delay === undefined) {
 			setState(newState);
 		} else {
@@ -19,14 +19,14 @@ export const useDelayedState = <T>(
 				timeoutRef.current = null;
 			}, delay);
 		}
-	};
+	}, []);
 
-	const cancelSetState = () => {
+	const cancelSetState = useCallback(() => {
 		if (timeoutRef.current) {
 			clearTimeout(timeoutRef.current);
 			timeoutRef.current = null;
 		}
-	};
+	}, []);
 
 	useEffect(() => {
 		return () => {
