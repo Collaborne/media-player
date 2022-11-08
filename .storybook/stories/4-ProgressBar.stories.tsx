ce63ac1c38c21710595923bb3ed38e96191d4eb4
Bragaru Ion <bragaruion@gmail.com>
2022-11-08 22:41:15 +0300

    Add boolean to control `<ProgressBar />` presence in `<Controls />`

--- .storybook/stories/4-ProgressBar.stories.tsx
+++ .storybook/stories/4-ProgressBar.stories.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import { Story } from '@storybook/react';
 
 import { BottomControls } from '../../src/components/bottom-controls/BottomControls';
 import { Controls } from '../../src/components/controls/Controls';
 import { ProgressBar as ProgressBarComponent } from '../../src/components/progress-bar/ProgressBar';
 import { withCorePlayer, withDemoCard } from '../decorators';
 
-export const ProgressBar: React.FC = () => {
+interface ProgressBarStoryProps {
+	hideAllControls: boolean;
+}
+
+export const ProgressBar: Story<ProgressBarStoryProps> = args => {
 	return (
-		<>
-			<Controls>
-				<BottomControls>
-					<ProgressBarComponent />
-				</BottomControls>
-			</Controls>
-		</>
+		<Controls hideAllControls={args.hideAllControls}>
+			<BottomControls>
+				<ProgressBarComponent />
+			</BottomControls>
+		</Controls>
 	);
 };
 
@@ -21,6 +23,20 @@ export default {
 	title: 'Media Player Controls',
 	component: ProgressBar,
 	decorators: [withCorePlayer, withDemoCard],
+	args: {
+		hideAllControls: false,
+	},
+	argTypes: {
+		hideAllControls: {
+			name: 'hideAllControls',
+			description:
+				'In `<MediaPlayer />` - we always display a `<ProgressBar/>`(all controls can be hidden, except `<ProgressBar />`) This boolean will enforce do not to display all UI Controls(if it is the case)',
+			table: {
+				type: { summary: 'boolean' },
+				defaultValue: { summary: false },
+			},
+		},
+	},
 	parameters: {
 		controls: { expanded: true },
 	},
--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -10,6 +10,11 @@ import { useControlsStyles } from './useControlsStyles';
 export interface ControlProps {
 	children: ReactNode;
 	className?: string;
+	/**
+	 * In `<MediaPlayer />` - we always display a `<ProgressBar/>`(all controls can be hidden, except `<ProgressBar />`)
+	 * This boolean will enforce do not to display all UI Controls(if it's the case)
+	 */
+	hideAllControls?: boolean;
 }
 
 /**
@@ -17,7 +22,11 @@ export interface ControlProps {
  * @category React Component
  * @category UI Controls
  */
-export const Controls: FC<ControlProps> = ({ children, className }) => {
+export const Controls: FC<ControlProps> = ({
+	children,
+	className,
+	hideAllControls = false,
+}) => {
 	const isAudio = useIsAudio();
 	const showControls = useMediaStore(state => state.showControls);
 
@@ -28,8 +37,11 @@ export const Controls: FC<ControlProps> = ({ children, className }) => {
 		classes: { bottomControls },
 	} = useBottomControlsStyles();
 
-	// Only <ProgressBar/> should be present if Controls components are not shown
+	if (!showControls && hideAllControls) {
+		return null;
+	}
 	if (!showControls && !isAudio) {
+		// Only <ProgressBar/> should be present if Controls components are not shown
 		return (
 			<div className={classNameControls}>
 				<div className={bottomControls}>
