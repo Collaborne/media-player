8e9733e8460fb23b00d5c92adbf62e2110b42881
Bragaru Ion <bragaruion@gmail.com>
2022-11-11 16:17:58 +0300

    Add tests for mediaType

--- src/store/__tests__/media-store.spec.tsx
+++ src/store/__tests__/media-store.spec.tsx
@@ -1,6 +1,21 @@
+import { ExternalStateUpdater } from '../../components/core-player/components/ExternalStateUpdater';
 import { MediaProviderProps, useMediaStore } from '../../context';
 import { useMediaListener } from '../../hooks';
+import { MediaType } from '../../types';
 import { act, setupMediaProvider } from '../../utils/testing-render';
+
+const setupMediaType = (mediaProviderProps: {
+	isAudio: boolean;
+	mediaType: MediaType;
+}) => {
+	const setupResults = setupMediaProvider(
+		<ExternalStateUpdater
+			isAudio={mediaProviderProps.isAudio}
+			mediaType={mediaProviderProps.mediaType}
+		/>,
+	);
+	return setupResults;
+};
 
 const setupHandleProgress = (
 	mediaProviderProps?: Partial<MediaProviderProps>,
@@ -45,6 +60,28 @@ describe('media-store', () => {
 			expect(events.includes('onTimeAlarm')).toBeFalsy();
 			act(() => mediaStore._handleProgress(3.01));
 			expect(events.includes('onTimeAlarm')).toBeTruthy();
+		});
+	});
+	describe('mediaType', () => {
+		it('initialization', () => {
+			const { mediaStore } = setupMediaType({
+				mediaType: 'audio',
+				isAudio: true,
+			});
+			expect(mediaStore.mediaType).toBe('audio');
+			expect(mediaStore.isAudio).toBeTruthy();
+		});
+		it('updating', () => {
+			const { mediaStore } = setupMediaType({
+				mediaType: 'audio',
+				isAudio: true,
+			});
+			act(() => mediaStore.setMediaType('video'));
+			expect(mediaStore.mediaType).toBe('video');
+			act(() => mediaStore.setIsAudio(false));
+			expect(mediaStore.isAudio).toBeFalsy();
+			act(() => mediaStore.setMediaType('unknown'));
+			expect(mediaStore.mediaType).toBe('unknown');
 		});
 	});
 });
