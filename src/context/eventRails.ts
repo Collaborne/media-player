3117f5d4228f04f5d347f172b320b809b304d641
Bragaru Ion <bragaruion@gmail.com>
2022-11-15 09:33:35 +0300

    Fix comment

--- src/context/eventRails.ts
+++ src/context/eventRails.ts
@@ -9,7 +9,7 @@ export interface EventRailsContext {
 	getHighlightColorBlended: BlendColors;
 }
 const NO_OP = () => undefined;
-/** Context for storing `Highlight` to distribute it to UI Controls
+/** Context for storing `EventRailsContext` to distribute it to `<EventRails />`
  * @category React Context
  */
 export const EventRailsContext = createContext<EventRailsContext>({
